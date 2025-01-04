3299c6ff16be2877c9536f71214fc0e22cfaf6a5
root <root@DESKTOP-CSPP1ON.localdomain>
2025-01-04 11:56:19 +0800

    0104

--- ignition/modules/deploy.js
+++ ignition/modules/deploy.js
@@ -2,15 +2,44 @@
 // async main
 // main
 
-const { ethers } = require("hardhat");
+const { ethers, run, network } = require("hardhat");
 
 async function main() {
   const SimpleSotrageFactory = await ethers.getContractFactory("SimpleStorage");
 
   console.log("Deploying SimpleStorage...");
   const simpleStorage = await SimpleSotrageFactory.deploy();
-  // await simpleStorage.deployed();
   console.log(`Deploy contract to : ${simpleStorage.target}`);
+  // console.log(network.config);
+  if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+    console.log("Waiting for 6 confirmations...");
+    await simpleStorage.deploymentTransaction().wait(6);
+    await verify(simpleStorage.target, []);
+  }
+  const currentValue = await simpleStorage.retrieve();
+  console.log("Current value of the contract:", currentValue.toString());
+
+  //update current value
+  const transactionResponse = await simpleStorage.store(7);
+  await transactionResponse.wait(1);
+  const updatedValue = await simpleStorage.retrieve();
+  console.log("Updated value of the contract:", updatedValue.toString());
+}
+
+async function verify(contractAddress, args) {
+  console.log("Verifying contract...");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (e) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("Contract already verified!");
+    } else {
+      console.log(e);
+    }
+  }
 }
 
 main()
---
+++ tasks/block-number.js
@@ -0,0 +1,12 @@
+const {
+  TASK_COMPILE_GET_REMAPPINGS,
+} = require("hardhat/builtin-tasks/task-names");
+const { task } = require("hardhat/config");
+
+task("block-number", "Prints the current block number").setAction(
+  async (taskArgs, hre) => {
+    const blockNumber = await hre.ethers.provider.getBlockNumber();
+    console.log("Current block number: ", blockNumber);
+  }
+);
+module.exports = {};
